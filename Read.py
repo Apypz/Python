d7160e1dff6eb8ff1457c175dd23fdc31abcfab8
Afif Nur Azam <113951759+Apypz@users.noreply.github.com>
2023-12-11 11:46:05 +0700

    Add files via upload

---
+++ Read.py
@@ -0,0 +1,21 @@
+# ------- Membuka, Membaca, dan Menutup isi dalam file ------- ##
+# try:
+#     myFile = open("H:\Hey.txt", "r")
+#     data = myFile.read()
+#     print(data)
+#     myFile.close()
+# except FileExistsError:
+#     print("File Tidak ada")
+
+## ------- Membuka, Membaca, dan Menutup isi dalam file ------- ##
+# try:
+#     myFile = open("H:\Hey.txt", "a+")
+#     myFile.write("Topi bundar")
+#     myFile.seek(0)
+#     data = myFile.read()
+#     print(data)
+#     myFile.close()
+# except FileExistsError:
+#     print("File Tidak ada")
+
+# Ctrl + "/" untuk menghapus pagar
